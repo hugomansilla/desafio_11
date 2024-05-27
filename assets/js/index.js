9de26029649eb923275a0f8b88c1ecb815c042fb
Hugo Mansilla <hugomansilla@gmail.com>
2024-05-26 22:23:07 -0400

    Se realizan cambios finales

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,32 @@
+// Precio inicial
 precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+
+// Contador 
+let count = 0;
+
+// Precio final 
+function precioFinal() {
+    const total = count*precio;
+    document.querySelector(".valor-total").innerHTML = total;
+}
+
+// Subir contador
+function subircontador() {
+    count++;
+    document.querySelector(".cantidad").innerHTML = count;
+    precioFinal();
+}
+
+document.querySelector(".buttonmas").onclick = subircontador
+
+// Bajar contador
+function bajarcontador() {
+    count--;
+    document.querySelector(".cantidad").innerHTML = count;
+    precioFinal();
+}
+
+document.querySelector(".buttonmenos").onclick = bajarcontador
